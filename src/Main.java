32f73e440987b781e81c1d3d93989fbb034a416d
Salma Ahmed <64385957+SalmaAhmed20@users.noreply.github.com>
2021-12-27 22:31:26 +0200

    Init

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String baseUri = "http://www.semanticweb.org/20180122-20180078-20180040/ontologies/2021/11/Project2#";
         OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM );
-        //NameSpace for base uri
-        //model.setNsPrefix("TER",baseUri);
         //---------City Class---------
         OntClass City = model.createClass(baseUri+"City" );
         //----- Data Property of city
-        DatatypeProperty cityName = model.createDatatypeProperty("CityName");
+        DatatypeProperty cityName = model.createDatatypeProperty(baseUri+"CityName");
         cityName.addDomain(City);
         cityName.addRange(XSD.xstring);
-        DatatypeProperty latitude = model.createDatatypeProperty("Latitude");
-        cityName.addDomain(City);
-        cityName.addRange(XSD.xfloat);
+        DatatypeProperty latitude = model.createDatatypeProperty(baseUri+"Latitude");
+        latitude.addDomain(City);
+        latitude.addRange(XSD.xfloat);
+        DatatypeProperty longitude = model.createDatatypeProperty(baseUri+"longitude");
+        longitude.addDomain(City);
+        longitude.addRange(XSD.xfloat);
         //for write on owl file
         FileWriter writer = new FileWriter("TERLines.owl");
         model.write(writer);
