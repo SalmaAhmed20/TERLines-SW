fcaab7afbef98372b8e8070684dda97ab15766bd
Salma Ahmed <64385957+SalmaAhmed20@users.noreply.github.com>
2021-12-28 00:41:31 +0200

    individuals of stops

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.vocabulary.RDFS;
 import org.apache.jena.vocabulary.XSD;
 
@@ -28,10 +30,10 @@ public class Main {
 
         //---------Stops,StopArea,StopPoints Classes---------
         OntClass Stops = model.createClass(baseUri + "Stops");
-        OntClass StopArea = model.createClass( baseUri + "StopArea" );
-        OntClass StopPoints = model.createClass( baseUri + "StopPoints" );
-        model.add(StopArea,RDFS.subClassOf,Stops);
-        model.add(StopPoints,RDFS.subClassOf,Stops);
+        OntClass StopArea = model.createClass(baseUri + "StopArea");
+        OntClass StopPoints = model.createClass(baseUri + "StopPoints");
+        model.add(StopArea, RDFS.subClassOf, Stops);
+        model.add(StopPoints, RDFS.subClassOf, Stops);
         //----- Data Property of Stops ---
         DatatypeProperty stopId = model.createDatatypeProperty(baseUri + "stopId");
         stopId.addDomain(Stops);
@@ -46,17 +48,58 @@ public class Main {
         stopsLong.addDomain(Stops);
         stopsLong.addRange(XSD.xfloat);
         //----ObjectProperty-----
-        ObjectProperty isParentStation = model.createObjectProperty( baseUri + "isParentStation" );
+        ObjectProperty isParentStation = model.createObjectProperty(baseUri + "isParentStation");
         isParentStation.addDomain(StopArea);
         isParentStation.addRange(StopPoints);
-        var cities = read_csv();
+        var cities = readFile("Dataset/cities.csv");
         for (int i = 1; i < cities.size(); i++) {
-            System.out.println(cities.get(i)[1]);
             var individual = City.createIndividual(baseUri + cities.get(i)[0].substring(cities.get(i)[0].lastIndexOf('/') + 1));
             individual.addProperty(cityName, cities.get(i)[0]);
-            individual.addProperty(latitude, cities.get(i)[1]);
-            individual.addProperty(longitude, cities.get(i)[2]);
+            model.add(individual, latitude, ResourceFactory.createTypedLiteral(cities.get(i)[1], XSDDatatype.XSDfloat));
+            model.add(individual, longitude, ResourceFactory.createTypedLiteral(cities.get(i)[2], XSDDatatype.XSDfloat));
+//            individual.addProperty(longitude, cities.get(i)[2]);
         }
+        var stops = readFile("Dataset/stops.txt");
+        for (int i = 1; i < stops.size();i++ ) {
+//            for (int j = 0; j < stops.get(i).length; j++) {
+//                if(!stops.get(i)[j].equals(""))
+//                    System.out.print(stops.get(i)[j]+",");
+//            }
+//            System.out.println();
+            if (stops.get(i)[0].contains("StopArea")) {
+                var area = StopArea.createIndividual
+                        (baseUri + stops.get(i)[0].substring(0, stops.get(i)[0].lastIndexOf(":")) + "_" + stops.get(i)[1]);
+                area.addProperty(stopId, stops.get(i)[0].substring(stops.get(i)[0].lastIndexOf(":") + 1));
+                area.addProperty(stopsName, stops.get(i)[1]);
+                if (stops.get(i)[2].equals("")) {
+                    model.add(area, stopsLat, ResourceFactory.createTypedLiteral(stops.get(i)[3], XSDDatatype.XSDfloat));
+                    model.add(area, stopsLong, ResourceFactory.createTypedLiteral(stops.get(i)[4], XSDDatatype.XSDfloat));
+                } else {
+                    model.add(area, stopsLat, ResourceFactory.createTypedLiteral(stops.get(i)[2], XSDDatatype.XSDfloat));
+                    model.add(area, stopsLong, ResourceFactory.createTypedLiteral(stops.get(i)[3], XSDDatatype.XSDfloat));
+                }
+                for (int j = i + 1; j < stops.size(); j++) {
+                    System.out.println(j);
+                    if (stops.get(j)[0].contains("StopPoint")) {
+                        var point = StopPoints.createIndividual
+                                (baseUri + stops.get(j)[0].substring(0, stops.get(j)[0].lastIndexOf(":")) + "_" + stops.get(j)[1]);
+                        point.addProperty(stopId, stops.get(j)[0].substring(stops.get(j)[0].lastIndexOf(":") + 1));
+                        point.addProperty(stopsName, stops.get(j)[1]);
+                        if (stops.get(i)[2].equals("")) {
+                            model.add(point, stopsLat, ResourceFactory.createTypedLiteral(stops.get(j)[3], XSDDatatype.XSDfloat));
+                            model.add(point, stopsLong, ResourceFactory.createTypedLiteral(stops.get(j)[4], XSDDatatype.XSDfloat));
+                        } else {
+                            model.add(point, stopsLat, ResourceFactory.createTypedLiteral(stops.get(j)[2], XSDDatatype.XSDfloat));
+                            model.add(point, stopsLong, ResourceFactory.createTypedLiteral(stops.get(j)[3], XSDDatatype.XSDfloat));
+                        }
+                        area.addProperty(isParentStation, point);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
 
         //for write on owl file
         FileWriter writer = new FileWriter("TERLines.owl");
@@ -64,11 +107,11 @@ public class Main {
         writer.close();
     }
 
-    static public Vector<String[]> read_csv() throws IOException {
-        String line = "";
+    static public Vector<String[]> readFile(String path) throws IOException {
+        String line;
         Vector<String[]> cities = new Vector<String[]>();
         String[] columns;
-        BufferedReader br = new BufferedReader(new FileReader("Dataset/cities.csv"));
+        BufferedReader br = new BufferedReader(new FileReader(path));
         while ((line = br.readLine()) != null) {
             columns = line.split(",");
             cities.add(columns);
